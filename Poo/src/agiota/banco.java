c277c02951c08cee80c741d48cbe331174df8d28
Nando <20nandogomes06@gmail.com>
2025-04-17 21:37:52 -0300

    thank you

---
+++ Poo/src/agiota/banco.java
@@ -0,0 +1,52 @@
+package agiota;
+
+import java.util.Scanner;
+
+public class banco {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String [] args){
+        emprestimo agiota = new emprestimo();
+
+        System.out.println("qual é o valor do emprestimo");
+        agiota.setValor(sc.nextInt());
+        System.out.println("quando foi deito o emprestimo");
+        agiota.setDataEmprestimo(sc.nextInt());
+        System.out.println("quantos é a parcela do emprestimo");
+        agiota.setNumPracelas(sc.nextInt());
+
+        System.out.println(agiota.toString());
+
+        emprestimo agio = new emprestimo();
+
+        System.out.println("qual é o valor do emprestimo");
+        agio.setValor(sc.nextInt());
+        System.out.println("quando foi deito o emprestimo");
+        agio.setDataEmprestimo(sc.nextInt());
+        System.out.println("quantos é a parcela do emprestimo");
+        agio.setNumPracelas(sc.nextInt());
+
+        System.out.println(agio.toString());
+
+        emprestimo ta = new emprestimo();
+
+        System.out.println("qual é o valor do emprestimo");
+        ta.setValor(sc.nextInt());
+        System.out.println("quando foi deito o emprestimo");
+        ta.setDataEmprestimo(sc.nextInt());
+        System.out.println("quantos é a parcela do emprestimo");
+        ta.setNumPracelas(sc.nextInt());
+
+        System.out.println(ta.toString());
+
+        emprestimo agi = new emprestimo();
+
+        System.out.println("qual é o valor do emprestimo");
+        agi.setValor(sc.nextInt());
+        System.out.println("quando foi deito o emprestimo");
+        agi.setDataEmprestimo(sc.nextInt());
+        System.out.println("quantos é a parcela do emprestimo");
+        agi.setNumPracelas(sc.nextInt());
+
+        System.out.println(agi.toString());
+    }
+}
---
+++ Poo/src/contaBancaria/saldo.java
@@ -0,0 +1,26 @@
+package contaBancaria;
+
+import java.util.Scanner;
+
+public class saldo {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[]args){
+        ContaBancaria saldo = new ContaBancaria();
+
+        System.out.println("digite o numero da conta");
+        saldo.setNumeroContaUm(sc.nextInt());
+        System.out.println("digite o saldo da conta");
+        saldo.setContaUm(sc.nextInt());
+
+        System.out.println(saldo.toString());
+
+        ContaBancaria saldo2 = new ContaBancaria();
+
+        System.out.println("digite o numero da conta");
+        saldo2.setNumeroContaDoesh(sc.nextInt());
+        System.out.println("digite o saldo da conta");
+        saldo2.setContaDoesh(sc.nextInt());
+
+        System.out.println(saldo2.toString());
+    }
+}
