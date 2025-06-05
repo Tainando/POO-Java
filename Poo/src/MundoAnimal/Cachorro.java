e65751768e7782e5a85a68b9c7e2fdb9e5debb16
Nando <20nandogomes06@gmail.com>
2025-06-04 21:06:08 -0300

    caracas, bagulho dificil

---
+++ Poo/src/MundoAnimal/Cachorro.java
@@ -0,0 +1,26 @@
+package MundoAnimal;
+
+public abstract class Cachorro extends Animal{
+    private String raca;
+
+    public Cachorro(String nome, int idade, String raca){
+    super(nome, idade);
+    this.raca = raca;
+    }
+    public String getRaca(){
+        return raca;
+    }
+    public void setRaca(String raca){
+        this.raca = raca;
+    }
+    public String emitirSom() {
+        return ("au au");
+    }
+    public String comer(){
+        return ("o cahorro come ração");
+    }
+    @Override
+    public void exibirinformaoees(String nome, int idade) {
+        System.out.println("O nome do cachorro é" + nome+ "a idade do cachorro é:" +idade+ "e a raça do cachorro é" +raca+".");;
+    }
+}
--- Poo/src/empresa/colaborador.java
+++
@@ -1,9 +0,0 @@
-package empresa;
-
-
-public class colaborador {
-    public static void main(String[] args) {
-        funcionario clt;
-}
-public void
-
